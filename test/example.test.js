0889a503ee033f16bef288081c4d7ba818f7e6cd
Alice Hsing <alicehsing@gmail.com>
2022-01-11 22:17:53 -0800

    TDD PURE renderParticipant(participant)

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,22 @@
 // IMPORT MODULES under test here:
-// import { example } from '../example.js';
+import { renderParticipant } from '../render-utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('renderParticipant function should take in a participant argument consisted of a name, then return a DOM node with a <p> element', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const participant = {
+        name: 'Lila',
+    };
+
+    const expected = `<p class="participant">Lila</p>`;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderParticipant(participant);
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.outerHTML, expected, 'returns a DOM node consisted of a <p> element');
 });
